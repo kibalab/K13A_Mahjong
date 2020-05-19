180319db99b3ad61005df2b992b81d7bf64bebc1
kjh030529 <kjh030529@gmail.com>
2020-05-19 11:59:57 +0900

    랜덤스폰 구현중

--- Assets/UdonScript/CardManager.cs
+++ Assets/UdonScript/CardManager.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -6,32 +5,61 @@ using VRC.Udon;
 
 public class CardManager : UdonSharpBehaviour
 {
+    public string positionName;
     private int cardCount = 13;
-    public GameObject[] cards = new GameObject[13];
-    public GameObject[] cardSprites = new GameObject[13];
+    public CardComponent[] cards;
+    public GameObject[] cardPoint = new GameObject[14];
     void Start()
     {
-        findCards();
-        Pickupable(false);
+        findPoints();
     }
-
-    void findCards()
+    GameObject[] findPoints()
     {
         //배열의 0~13 은 소유카드 14는 추가카드
         for (int i = 0; i <= cardCount; i++)
         {
-            cards[i] = this.gameObject.transform.GetChild(i).gameObject;
+            Debug.Log(this.gameObject.transform.GetChild(i).name);
+            cardPoint[i] = this.gameObject.transform.GetChild(i).gameObject;
+        }
+        return cardPoint;
+    }
+
+    public void setCards()
+    {
+        for(int i = 0; i<= cardCount; i++)
+        {
+            cards[i].SetPosition(cardPoint[i].transform.position, cardPoint[i].transform.rotation); 
         }
     }
 
     public void Pickupable(bool b)
     {
-        foreach (GameObject card in cards)
+        foreach (CardComponent card in cards)
         {
-            card.GetComponent<BoxCollider>().enabled = b;
+            card.gameObject.GetComponent<BoxCollider>().enabled = b;
         }
     }
-    public void SetCard(string name, int index)
+    public CardComponent[] sortCard()
     {
+        int i;
+        int j;
+        CardComponent temp;
+
+        for (i = (cards.Length - 1); i >= 0; i--)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                if (cards[j - 1].normalCardNumber > cards[j].normalCardNumber)
+                {
+                    temp = cards[j - 1];
+                    cards[j - 1] = cards[j];
+                    cards[j] = temp;
+                }
+            }
+        }
+        return cards;
     }
+
+
+
 }
