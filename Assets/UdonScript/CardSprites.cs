477d083a7fdfdf21a6dace1928415df8baa9d3fe
Beretta <kirnot92@gmail.com>
2020-05-23 11:05:42 +0900

    아냐 차라리 없을 때 오류 나는게 나아...

--- Assets/UdonScript/CardSprites.cs
+++ Assets/UdonScript/CardSprites.cs
@@ -9,11 +9,8 @@ public class CardSprites : UdonSharpBehaviour
     public Sprite FindSprite(string spriteName)
     {
         var spriteGameObject = transform.Find(spriteName);
-        if (spriteGameObject == null) { return null; }
-
         var spriteRenderer = spriteGameObject.GetComponent<SpriteRenderer>();
-        if (spriteRenderer == null) { return null; }
-        
+
         return spriteRenderer.sprite;
     }
 }
