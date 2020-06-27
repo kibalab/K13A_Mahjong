6eb4086ec8ec13f9025fb5ba9a7097bc8343a458
Beretta <kirnot92@gmail.com>
2020-06-28 00:01:56 +0900

    InputEvent에 여러 번 눌렀을 때 오동작 방지를 위한 InputBlock 추가

--- Assets/UdonScript/InputEvent.cs
+++ Assets/UdonScript/InputEvent.cs
@@ -6,6 +6,7 @@ using VRC.Udon;
 
 public class InputEvent : UdonSharpBehaviour
 {
+    public bool isInputBlocked = false;
     public string EventType;
     public int PlayerIndex;
     public int DiscardedCardYamaIndex;
@@ -13,22 +14,38 @@ public class InputEvent : UdonSharpBehaviour
 
     public void SetUIEvent(string uiName, int playerIndex)
     {
-        EventType = uiName;
-        PlayerIndex = playerIndex;
+        if (!isInputBlocked)
+        {
+            EventType = uiName;
+            PlayerIndex = playerIndex;
+        }
     }
 
     public void SetDiscardEvent(int yamaIndex, string eventType, int playerIndex)
     {
-        DiscardedCardYamaIndex = yamaIndex;
-        EventType = eventType;
-        PlayerIndex = playerIndex;
+        if (!isInputBlocked)
+        {
+            DiscardedCardYamaIndex = yamaIndex;
+            EventType = eventType;
+            PlayerIndex = playerIndex;
+        }
     }
 
     public void SetChiEvent(Vector2 chiIndex, string eventType, int playerIndex)
     {
-        ChiIndex = new Vector2(chiIndex.x, chiIndex.y);
-        EventType = eventType;
-        PlayerIndex = playerIndex;
+        if (!isInputBlocked)
+        {
+            ChiIndex = new Vector2(chiIndex.x, chiIndex.y);
+            EventType = eventType;
+            PlayerIndex = playerIndex;
+        }
+    }
+
+    public void SetInputBlock(bool active)
+    {
+        // 여러 번 누르면 큐잉된 InputAction의 값 자체가 바뀔 수 있다
+        // 그래서 한 번 눌러서 큐잉됐을 때 input을 막기 위한 용도
+        isInputBlocked = active;
     }
 
     public void Clear()
@@ -37,5 +54,7 @@ public class InputEvent : UdonSharpBehaviour
         EventType = "";
         PlayerIndex = -1;
         ChiIndex = Vector2.zero;
+
+        SetInputBlock(false);
     }
 }
